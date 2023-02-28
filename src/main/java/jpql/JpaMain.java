52b1fe8ec6f52c6882d44c4d3feec03c20f04841
josoya <thdi4564@nate.com>
2023-02-28 14:20:37 +0900

    jpql 경로 표현식 (묵시적 조인)

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,6 +1,8 @@
 package jpql;
 
 import javax.persistence.*;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class JpaMain {
@@ -15,28 +17,51 @@ public class JpaMain {
             team.setName("teamA");
             em.persist(team);
 
-            Member member = new Member();
-            member.setUsername("관리자");
-            member.setAge(10);
-            member.setType(MemberType.ADMIN);
+            Member member1 = new Member();
+            member1.setUsername("관리자1");
+            member1.setTeam(team);
+            em.persist(member1);
 
-            member.setTeam(team);
+            Member member2 = new Member();
+            member2.setUsername("관리자2");
+            member2.setTeam(team);
+            em.persist(member2);
 
-            em.persist(member);
+//            member.setTeam(team);
 
             em.flush();
             em.clear();
 
+            /**
+             * 명시적 조인 - join 키워드를 직접 사용
+             * 묵시적 조인
+             * - join 키워드를 안쓰고 조인을 사용
+             * - 항상 내부 조인
+             * - 한눈에 파악하기 힘듬
+             * - 실무에서 사용하지 말아야한다.
+             */
+            // 실무에서는 절대로 묵시적 내부 조인쓰지 말자.
 
-            String query = "select group_concat('group_concat', m.username) from Member  m";
+            // 사용자 정의 함수
+//            String query = "select group_concat('group_concat', m.username) from Member  m";
 
-            List<String> resultList = em.createQuery(query, String.class).getResultList();
+            // 묵시적이 내부 조인 발생, 탐색 0
+//            String query = "select m.team from Member m";
 
-            for (String s : resultList) {
-                System.out.println("s = " + s);
-            }
+            // 실패한 쿼리
+//            String query = "select t.members.username from Team t";
+
+            // 묵시적 내부 조인 발생, 탐색 X , 명시적 조인을 가지고 와서 성공
+            String query = "select m.username from Team t join t.members m";
+
+
+            List<Collection> result = em.createQuery(query, Collection.class)
+                    .getResultList();
+            System.out.println("result() = " + result);
+
 
             tx.commit();
+
         } catch (Exception e) {
             e.printStackTrace();
             tx.rollback();
